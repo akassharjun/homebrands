e172fc75d25d188736b7247b4ded17d526939ee1
Akassharjun Shanmugarajah <akassharjun@gmail.com>
2019-09-29 11:59:59 +0530

    added base order page.

---
+++ lib/pages/order.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class OrderPage extends StatelessWidget {
+	final String name;
+	final int quantity;
+
+  const OrderPage({Key key, this.name, this.quantity}) : super(key: key);
+	
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+	
+}
