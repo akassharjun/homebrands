9c15524e19442150dadff57730fa05936849d199
Akassharjun Shanmugarajah <akassharjun@gmail.com>
2019-10-20 13:03:50 +0530

    done favourites page

--- lib/pages/clean/dashboard.dart
+++ lib/pages/clean/dashboard.dart
@@ -9,10 +9,10 @@ import 'package:homebrands/model/shop.dart';
 import 'package:homebrands/pages/clean/place_order.dart';
 import 'package:homebrands/pages/clean/shop_details.dart';
 import 'package:homebrands/utils/product_data.dart';
+import 'package:homebrands/utils/screen_util.dart';
 import 'package:homebrands/widgets/bloc_error_handling.dart';
 import 'package:homebrands/widgets/heading.dart';
 import 'package:homebrands/widgets/progress_indicator.dart';
-import 'package:homebrands/utils/screen_util.dart';
 
 import '../../constants.dart';
 
@@ -22,7 +22,6 @@ class DashboardPage extends StatefulWidget {
 }
 
 class _DashboardPageState extends State<DashboardPage> {
-
   int featuredCarouselIndex = 0;
   int trendingCarouselIndex = 0;
 
@@ -30,7 +29,6 @@ class _DashboardPageState extends State<DashboardPage> {
 
   @override
   void initState() {
-
     dashboardBloc.dispatch(FetchFeaturedShops());
     dashboardBloc.dispatch(FetchTrendingShops());
     super.initState();
@@ -43,10 +41,12 @@ class _DashboardPageState extends State<DashboardPage> {
         child: Column(
           children: <Widget>[
             Heading.buildTitle("Trending Items"),
+            SizedBox(height: ScreenUtil.getHeight(0.5)),
             buildTrendingShopList(trendingProductItemDashboard),
+            SizedBox(height: ScreenUtil.getHeight(2)),
             Heading.buildTitle("Featured Shops"),
+            SizedBox(height: ScreenUtil.getHeight(0.5)),
             buildFeaturedShopList(featuredDashboardShops)
-
           ],
         ),
       ),
@@ -107,7 +107,6 @@ class _DashboardPageState extends State<DashboardPage> {
     );
   }
 
-
   Widget buildTrendingShopList(List<Product> product) {
     var items = trendingProductItemDashboard.toList().map((i) {
       return Builder(builder: (BuildContext context) {
@@ -116,7 +115,7 @@ class _DashboardPageState extends State<DashboardPage> {
     }).toList();
 
     return CarouselSlider(
-      onPageChanged: (value){
+      onPageChanged: (value) {
         trendingCarouselIndex = value;
         print(trendingCarouselIndex);
       },
@@ -140,86 +139,80 @@ class _DashboardPageState extends State<DashboardPage> {
     return GestureDetector(
       onTap: () => routeTrending(trendingCarouselIndex),
       child: Container(
-        padding: ScreenUtil.getPadding(1,0),
-        height: ScreenUtil.getHeight(25),
-        width: double.infinity,
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: NetworkImage(product.thumbnail,),
-            fit: BoxFit.cover
-          )
-        ),
-        child: Stack(
-          children: <Widget>[
-            Positioned(
-              bottom: 0,
-              right: 0,
-              child: Container(
-                decoration: BoxDecoration(
-                  boxShadow: [
-                    BoxShadow(
-                      color: kAsh.withOpacity(0.3),
-                      blurRadius: 10
-                    )
-                  ],
-                  gradient: LinearGradient(colors: [kMagenta,Colors.red]),
-                    color: kWhite,
-                    borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(20),
-                    topLeft: Radius.circular(20),
-                  )
-                ),
-                height: ScreenUtil.getHeight(13),
-                width: ScreenUtil.getWidth(60),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text(
-                      product.name,
-                      style: TextStyle(
-                        color: kWhite,
-                        fontSize: ScreenUtil.getTextSize(13),
-                        fontWeight: FontWeight.w900
-                      ),
-                    ),
-                    Text(
-                      product.business,
-                      style: TextStyle(
-                        color: kWhite,
-                        fontSize: ScreenUtil.getTextSize(9.5),
-                        fontWeight: FontWeight.w300
-                      ),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Text(product.price.currency,
-                          style: TextStyle(
-                            color: kWhite,
-                            fontSize: ScreenUtil.getTextSize(12),
-                          ),
-                        ),
-                        Text(' '),
-                        Text(product.price.amount.toString(),
-                          style: TextStyle(
-                            color: kWhite,
-                            fontSize: ScreenUtil.getTextSize(12),
-                            fontWeight: FontWeight.w900
-                          ),
-                        ),
+          padding: ScreenUtil.getPadding(1, 0),
+          height: ScreenUtil.getHeight(25),
+          width: double.infinity,
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: NetworkImage(
+                    product.thumbnail,
+                  ),
+                  fit: BoxFit.cover)),
+          child: Stack(
+            children: <Widget>[
+              Positioned(
+                bottom: 0,
+                right: 0,
+                child: Container(
+                  decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(color: kAsh.withOpacity(0.3), blurRadius: 10)
                       ],
-                    ),
-                  ],
+                      gradient: LinearGradient(colors: [kMagenta, Colors.red]),
+                      color: kWhite,
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(20),
+                        topLeft: Radius.circular(20),
+                      )),
+                  height: ScreenUtil.getHeight(13),
+                  width: ScreenUtil.getWidth(60),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        product.name,
+                        style: TextStyle(
+                            color: kWhite,
+                            fontSize: ScreenUtil.getTextSize(13),
+                            fontWeight: FontWeight.w900),
+                      ),
+                      Text(
+                        product.business,
+                        style: TextStyle(
+                            color: kWhite,
+                            fontSize: ScreenUtil.getTextSize(9.5),
+                            fontWeight: FontWeight.w300),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text(
+                            product.price.currency,
+                            style: TextStyle(
+                              color: kWhite,
+                              fontSize: ScreenUtil.getTextSize(12),
+                            ),
+                          ),
+                          Text(' '),
+                          Text(
+                            product.price.amount.toString(),
+                            style: TextStyle(
+                                color: kWhite,
+                                fontSize: ScreenUtil.getTextSize(12),
+                                fontWeight: FontWeight.w900),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            )
-          ],
-        )
-      ),
+              )
+            ],
+          )),
     );
   }
 
-  Widget buildFeaturedShopList(List<Shop> shop){
+  Widget buildFeaturedShopList(List<Shop> shop) {
     var items = shop.toList().map((i) {
       return Builder(builder: (BuildContext context) {
         return _buildFeaturedShop(i);
@@ -227,7 +220,7 @@ class _DashboardPageState extends State<DashboardPage> {
     }).toList();
 
     return CarouselSlider(
-      onPageChanged: (value){
+      onPageChanged: (value) {
         featuredCarouselIndex = value;
         print(featuredCarouselIndex);
       },
@@ -253,43 +246,37 @@ class _DashboardPageState extends State<DashboardPage> {
         height: ScreenUtil.getHeight(2),
         width: ScreenUtil.getWidth(55),
         decoration: BoxDecoration(
-          borderRadius:BorderRadius.all(Radius.circular(5)),
-          gradient: LinearGradient(colors: getColours())
-        ),
+            borderRadius: BorderRadius.all(Radius.circular(5)),
+            gradient: LinearGradient(colors: getColours())),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
               shop.businessName,
               style: TextStyle(
-                color: kWhite,
-                fontWeight: FontWeight.w600,
-                fontSize: ScreenUtil.getTextSize(11.5)
-              ),
+                  color: kWhite,
+                  fontWeight: FontWeight.w600,
+                  fontSize: ScreenUtil.getTextSize(11.5)),
             ),
             Text(
               shop.category,
-              style: TextStyle(
-                color: kWhite,
-                fontWeight: FontWeight.w300
-              ),
+              style: TextStyle(color: kWhite, fontWeight: FontWeight.w300),
             ),
             SizedBox(
               height: ScreenUtil.getHeight(1),
             ),
             Container(
               decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(100)),
-                border: Border.all(
-                  color: kWhite,
-                )
-              ),
+                  borderRadius: BorderRadius.all(Radius.circular(100)),
+                  border: Border.all(
+                    color: kWhite,
+                  )),
               child: Padding(
-                padding: ScreenUtil.getPaddingAll(5),
+                padding: ScreenUtil.getPaddingAll(8.5),
                 child: Text(
                   shop.paymentMethod,
                   style: TextStyle(
-                      color: kWhite,
+                    color: kWhite,
                   ),
                 ),
               ),
@@ -306,28 +293,33 @@ class _DashboardPageState extends State<DashboardPage> {
     super.dispose();
   }
 
-  List<Color> getColours(){
+  List<Color> getColours() {
     var range = Random();
     int index = range.nextInt(7);
-    if (index == 1){
+    if (index == 1) {
       return [Colors.red, Colors.pink];
-    }if (index == 2){
+    }
+    if (index == 2) {
       return [Colors.pink, Colors.purple];
-    }if (index == 3){
+    }
+    if (index == 3) {
       return [kMagenta, Colors.orange];
-    }if (index == 4){
+    }
+    if (index == 4) {
       return [Colors.amber, Colors.red];
-    }if (index == 5){
+    }
+    if (index == 5) {
       return [Colors.purpleAccent, Colors.blueAccent];
-    }if (index == 6){
+    }
+    if (index == 6) {
       return [Colors.blue, Colors.pink];
-    }if (index == 0){
+    }
+    if (index == 0) {
       return [Colors.purpleAccent, Colors.lightBlueAccent];
     }
-
   }
 
-  routeFeatured(int index){
+  routeFeatured(int index) {
     Navigator.push(
       context,
       MaterialPageRoute(
@@ -338,7 +330,7 @@ class _DashboardPageState extends State<DashboardPage> {
     );
   }
 
-  routeTrending(int index){
+  routeTrending(int index) {
     Navigator.push(
       context,
       MaterialPageRoute(
@@ -348,9 +340,4 @@ class _DashboardPageState extends State<DashboardPage> {
       ),
     );
   }
-
 }
-
-
-
-
--- lib/pages/clean/favourites.dart
+++ lib/pages/clean/favourites.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:homebrands/bloc/favorite/favorite_bloc.dart';
 import 'package:homebrands/model/product.dart';
+import 'package:homebrands/utils/screen_util.dart';
 import 'package:homebrands/widgets/bloc_error_handling.dart';
+import 'package:homebrands/widgets/heading.dart';
 import 'package:homebrands/widgets/progress_indicator.dart';
 
 class FavouritesPage extends StatefulWidget {
@@ -15,7 +17,109 @@ class _FavouritesPageState extends State<FavouritesPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            Heading.buildTitle("Favourites"),
+            SizedBox(height: ScreenUtil.getHeight(0.5)),
+            Card(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16.0)),
+              margin: EdgeInsets.all(16.0),
+              elevation: 2.0,
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: <Widget>[
+                        Column(
+                          children: <Widget>[
+//                            Container(
+//                              height: 50,
+//                              width: 50,
+//                              color: Colors.amber,
+//                            ),
+                            
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                "Yeh's Items",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w500, fontSize: 18),
+                              ),
+                            ),
+                            Row(
+                              children: <Widget>[
+                                Icon(Icons.star,
+                                    size: 16.0, color: Colors.amber),
+                                Icon(Icons.star,
+                                    size: 16.0, color: Colors.amber),
+                                Icon(Icons.star,
+                                    size: 16.0, color: Colors.amber),
+                                Icon(Icons.star,
+                                    size: 16.0, color: Colors.amber),
+                                Icon(Icons.star,
+                                    size: 16.0, color: Colors.amber)
+                              ],
+                            )
+                          ],
+                        ),
+                        Column(
+                          children: <Widget>[
+                            Row(
+                              children: <Widget>[
+                                Text("Price Range: "),
+                                SizedBox(width: ScreenUtil.getWidth(0.5)),
+                                Text("150 LKR")
+                              ],
+                            ),
+                            Row(
+                              children: <Widget>[
+                                Text("Delivery Time: "),
+                                SizedBox(width: ScreenUtil.getWidth(0.5)),
+                                Text("30 mins")
+                              ],
+                            ),
+                            SizedBox(height: ScreenUtil.getHeight(1)),
+                            Row(
+                              children: <Widget>[
+                                Icon(
+                                  Icons.check,
+                                  size: 16.0,
+                                  color: Colors.green,
+                                ),
+                                SizedBox(width: ScreenUtil.getWidth(0.5)),
+                                Text("Pick up")
+                              ],
+                            ),
+                            Row(
+                              children: <Widget>[
+                                Icon(
+                                  Icons.check,
+                                  size: 16.0,
+                                  color: Colors.green,
+                                ),
+                                SizedBox(width: ScreenUtil.getWidth(0.5)),
+                                Text("Delivery")
+                              ],
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+//                    SizedBox(height: ScreenUtil.getHeight(2))
+                  ],
+                ),
+              ),
+            ),
+//            SizedBox(height: ScreenUtil.getHeight(0.5)),
+          ],
+        ),
+      ),
+    );
   }
 
   Widget _buildFavouriteListBuilder() {
